47d94c583237da70cd145f90479b66af5da31a30
Andrew Raim <andrew.raim@census.gov>
2025-09-26 16:14:25 -0400

    Work on VMF linear example

--- vignettes/examples/vmf/vmf-v3.cpp
+++ vignettes/examples/vmf/vmf-v3.cpp
@@ -9,7 +9,6 @@ Rcpp::List r_vmf_pre_v3(unsigned int n, double kappa, double d, unsigned int N,
 	vws::rejection_args args;
 	args.max_rejects = max_rejects;
 	args.report = report;
-	args.action = vws::error_action::STOP;
 
 	LinearVWSRegion supp(-1, 1, kappa, d);
 	vws::FMMProposal<double, LinearVWSRegion> h(supp);
