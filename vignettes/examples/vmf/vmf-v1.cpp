6532d04b527d993e8700b045414bdd832d4c291d
Andrew Raim <andrew.raim@gmail.com>
2025-10-13 11:29:19 -0400

    Updates to draft
    Combine preliminaries section into overview and add content.
    
    Change first example to Quarto annotation form.

--- vignettes/examples/vmf/vmf-v1.cpp
+++ vignettes/examples/vmf/vmf-v1.cpp
@@ -1,16 +1,17 @@
-// [[Rcpp::depends(vws, fntl)]]
-#include "vws.h"
-#include "texp.h"
+// [[Rcpp::depends(vws, fntl)]]                             // <1>
+#include "vws.h"                                            // <2>
+#include "texp.h"                                           // <3>
 
-// [[Rcpp::export]]
-Rcpp::List r_vmf_pre_v1(unsigned int n, double kappa, double d, unsigned int N,
-    double tol = 0, unsigned int max_rejects = 10000, unsigned int report = 10000)
+// [[Rcpp::export]]                                         // <4>
+Rcpp::List r_vmf_pre_v1(unsigned int n, double kappa, double d,
+	unsigned int N, double tol = 0, unsigned int max_rejects = 10000,
+	unsigned int report = 10000)
 {
-    vws::rejection_args args;
+    vws::rejection_args args;                               // <5>
     args.max_rejects = max_rejects;
     args.report = report;
 
-    const vws::weight_dfd& w =
+    const vws::weight_dfd& w =                              // <6>
     [&](double x, bool log = true) {
         double out = R_NegInf;
         if (std::fabs(x) < 1){
@@ -19,7 +20,7 @@ Rcpp::List r_vmf_pre_v1(unsigned int n, double kappa, double d, unsigned int N,
         return log ? out : std::exp(out);
     };
 
-    fntl::density df = [&](double x, bool log = false) {
+    fntl::density df = [&](double x, bool log = false) {    // <7>
         return d_texp(x, kappa, -1, 1, log);
     };
     fntl::cdf pf = [&](double q, bool lower = true, bool log = false) {
@@ -29,14 +30,14 @@ Rcpp::List r_vmf_pre_v1(unsigned int n, double kappa, double d, unsigned int N,
         return q_texp(p, kappa, -1, 1, lower, log);
     };
 
-    vws::UnivariateHelper helper(df, pf, qf);
-    vws::RealConstRegion supp(-1, 1, w, helper);
-    vws::FMMProposal<double, vws::RealConstRegion> h(supp);
+    vws::UnivariateHelper helper(df, pf, qf);               // <8>
+    vws::RealConstRegion supp(-1, 1, w, helper);            // <9>
+    vws::FMMProposal<double, vws::RealConstRegion> h(supp); // <10>
 
-    auto lbdd = h.refine(N - 1, tol);
-    auto out = vws::rejection(h, n, args);
+    auto lbdd = h.refine(N - 1, tol);                       // <11>
+    auto out = vws::rejection(h, n, args);                  // <12>
 
-    return Rcpp::List::create(
+    return Rcpp::List::create(                              // <13>
         Rcpp::Named("draws") = out.draws,
         Rcpp::Named("rejects") = out.rejects,
         Rcpp::Named("lbdd") = lbdd
