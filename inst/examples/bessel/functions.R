08c27bca94481b828b4d5acd7e2e7fa0d6a5bad4
Andrew Raim <andrew.raim@gmail.com>
2025-10-13 23:01:38 -0400

    Clean out stale examples

--- inst/examples/bessel/functions.R
+++
@@ -1,7 +0,0 @@
-d_bessel = function(x, a, nu, normalize = TRUE, log = FALSE)
-{
-	lnc = ifelse(normalize, log(besselI(a, nu)), 0)
-	out = (2*x + nu) * log(a/2) - lfactorial(x) - lgamma(x + nu + 1) - nc
-	if (log) { return(out) } else { return(exp(out)) }
-}
-
